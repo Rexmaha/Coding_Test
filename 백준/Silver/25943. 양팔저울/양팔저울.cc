ec2ac6928fae14a93447f603484d29e400d36ea8
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-02-21 02:19:13 +0900

    [Silver IV] Title: 양팔저울, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25943. 양팔저울/양팔저울.cc
+++ 백준/Silver/25943. 양팔저울/양팔저울.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
 typedef long long ll;
@@ -9,22 +8,17 @@ int main(){
     ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
     cin>>n;
 
-    vector<ll> v(n+1);
-    ll left=0, right=0;
-    for (int i = 1; i <= n; i++)
-    {
-        cin>>v[i];
+    ll num, left=0, right=0;
+    for (int i=1;i<=n;i++) {
+        cin>>num;
 
-        if(i==0 || left<=right) left+=v[i];
-        else if(i==1 || left>right) right+=v[i];
+        if(i==0 || left<=right) left+=num;
+        else if(i==1 || left>right) right+=num;
     }
-    //cout<<"left = "<<left<<", right = "<<right<<'\n';
 
-    ll remain=abs(left-right);
-    ll res=0;
-    vector<ll> chu={100, 50, 20, 10, 5, 2, 1};
+    ll remain=abs(left-right), res=0;
+    ll chu[7]={100, 50, 20, 10, 5, 2, 1};
     for(int i=0;i<7;i++){
-        //cout<<"i = "<<i<<", remain = "<<remain<<", res = "<<res<<'\n';
         if(remain>=chu[i]){
             res+=remain/chu[i];
             remain-=(remain/chu[i])*chu[i];
