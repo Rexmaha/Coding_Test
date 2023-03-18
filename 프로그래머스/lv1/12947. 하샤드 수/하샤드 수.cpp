c6320e0ab634b8d0d87e5898500c932bf0eed3f9
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-03-19 04:13:22 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    string s=to_string(x);
+    int sum=0;
+    for(int i=0;i<s.length();i++) sum+=(s[i]-'0');
+    
+    if(x%sum==0) return true;
+    else return false;
+}
