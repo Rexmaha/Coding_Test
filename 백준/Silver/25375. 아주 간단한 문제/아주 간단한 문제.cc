70488b27a706aa59734485e69784fb0d19b86e2f
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-04-15 20:43:46 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int Q; cin >> Q;
+	while (Q--) {
+		ll a, b; cin >> a >> b;
+		if (b % a == 0 && a < b) cout << 1 << '\n';
+		else cout << 0 << '\n';
+	}
+}
