ff3019d04b46fa1579e4ef8129261a6deb524fc8
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-04-19 13:39:25 +0900

    [Bronze II] Title: 진수 변환기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14915. 진수 변환기/진수 변환기.cc
+++ 백준/Bronze/14915. 진수 변환기/진수 변환기.cc
@@ -2,28 +2,29 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+string s;
+char mod;
+int m, n;
 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-    int m, n; cin>>m>>n;
-    string s="";
-    char mod;
-
-    while(m/n!=0){
-        mod=m%n;
-
-        if(mod>=10) mod+=55;
-        else mod+=48;
-        s+=mod;
-
-        m/=n;
-    }
+void getJari(){
     mod=m%n;
 
     if(mod>=10) mod+=55;
     else mod+=48;
     s+=mod;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin>>m>>n;
+    s="";
+
+    while(m/n!=0){
+        getJari();
+        m/=n;
+    }
+    getJari();
 
     reverse(s.begin(), s.end());
     cout<<s;
