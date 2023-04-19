f6406e148147987a0cb6854a94bdb6d466bdb62b
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-04-19 23:49:51 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    int n, m; cin>>n>>m;
+    vector<int> v(n+1, 0);
+    
+    for(int i=1;i<=n;i++) v[i]=i;
+
+    while(m--){
+        int begin, end, mid; cin>>begin>>end>>mid;
+
+        vector<int> front {&v[mid], &v[end+1]};
+        vector<int> back {&v[begin], &v[mid]};
+
+        for(int i=0;i<front.size();i++){
+            v[begin+i]=front[i];
+        }
+        for(int i=0;i<back.size();i++){
+            v[end-mid+1+begin+i]=back[i];
+        }
+    }
+    
+    for(int i=1;i<=n;i++) cout<<v[i]<<" ";
+}
