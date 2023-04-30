cc39623118e90d08af2558d38ccb6a3a495edc7f
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-05-01 00:01:19 +0900

    [Silver III] Title: I LOVE JavaScript, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/27969. I LOVE JavaScript/I LOVE JavaScript.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+typedef unsigned long long ull;
+stack<char> st;
+
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    string ason; getline(cin, ason);
+    ull res=0;
+    for(int i=0;i<ason.length();i++){
+        if(ason[i]=='[') {
+            st.push('[');
+            res+=8;
+        }
+        else if(ason[i]==']' && !st.empty()){
+            st.pop();
+        }
+        else if(ason[i]>='0' && ason[i]<='9'){
+            do{
+                ++i;
+            }while(ason[i]>='0' && ason[i]<='9');
+            res+=8;
+        }
+        else if((ason[i]>='a' && ason[i]<='z') || (ason[i]>='A' && ason[i]<='Z')){
+            string tmp="";
+            do{
+                tmp+=ason[i];
+                ++i;
+            }while((ason[i]>='a' && ason[i]<='z') || (ason[i]>='A' && ason[i]<='Z'));
+            res+=tmp.length()+12;
+        }
+    }
+    cout<<res;
+}
