d0a3e6eca385d77004ce5a5f5b2d7cfe36aa9add
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2023-06-09 14:32:20 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.cc
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -5,18 +5,15 @@ vector<int> v;
 int main(){
     ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
 
-    int tmp;
+    int tmp, sum=0, min=101;
     for(int i=0;i<7;i++){
         cin>>tmp;
-        if(tmp%2!=0) v.push_back(tmp);
-    }
-
-    if(v.size()==0) cout<<"-1";
-    else{
-        int sum=0;
-        for(int i=0;i<v.size();i++){
-            sum+=v[i];
+        if(tmp%2!=0) {
+            sum+=tmp;
+            
+            if(min>tmp) min=tmp;
         }
-        cout<<sum<<"\n"<<*min_element(v.begin(), v.end());
     }
+    if(sum==0) cout<<"-1";
+    else cout<<sum<<"\n"<<min;
 }
