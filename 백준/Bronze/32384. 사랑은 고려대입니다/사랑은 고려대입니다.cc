936c48bad984a563d80ec12785e24eb2cb8a901b
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2024-11-19 14:26:49 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    int n; cin>>n;
+
+    while(n--){
+        cout<<"LoveisKoreaUniversity ";
+    }
+}
