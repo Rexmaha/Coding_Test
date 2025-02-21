b91ccf3f24af060a4469ee5a23ac211025ee43c0
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2025-02-21 12:12:51 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int a, b; cin>>a>>b;
+    cout<<1000*a+10000*b;
+}
