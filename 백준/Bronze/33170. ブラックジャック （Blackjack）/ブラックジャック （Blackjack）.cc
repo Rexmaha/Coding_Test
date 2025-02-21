5e64702e5e8f578b09472555df9ab83ca8b0ce73
Rexmaha <37144160+Rexmaha@users.noreply.github.com>
2025-02-21 12:11:31 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    if(a+b+c<=21) cout<<1;
+    else cout<<0;
+}
